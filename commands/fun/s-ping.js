f2dac71588a67b2556102592ed11995effb4bcf4
ToastedExistence <thetruesilo@gmail.com>
2023-11-09 18:30:34 -0500

    Fix for this command again afgsdhik

--- commands/fun/s-ping.js
+++ commands/fun/s-ping.js
@@ -2,8 +2,8 @@ const { SlashCommandBuilder } = require('discord.js');
 
 module.exports = {
 	data: new SlashCommandBuilder()
-		.setName('Secret ping')
-		.setDescription('Replies only to you with Pong!'),
+		.setName('secret ping')
+		.setDescription('Replies to you only with Pong!'),
 	async execute(interaction) {
         await interaction.reply({ content: 'Secret Pong!', ephemeral: true });
 	},
