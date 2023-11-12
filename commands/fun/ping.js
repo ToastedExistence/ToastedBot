5bfd5c05cfa263bac78b9ef15213a89638a316a2
ToastedExistence <thetruesilo@gmail.com>
2023-11-12 13:56:09 -0500

    added a lock out to the bot

--- commands/fun/ping.js
+++ commands/fun/ping.js
@@ -1,8 +1,9 @@
-const { SlashCommandBuilder } = require('discord.js');
+const { SlashCommandBuilder, PermissionFlagsBits } = require('discord.js');
 
 module.exports = {
 	data: new SlashCommandBuilder()
 		.setName('ping')
+		.setDefaultMemberPermissions(PermissionFlagsBits.ManageWebhooks)
 		.setDescription('Replies with Pong!'),
 	async execute(interaction) {
 		await interaction.reply('Pong!');
